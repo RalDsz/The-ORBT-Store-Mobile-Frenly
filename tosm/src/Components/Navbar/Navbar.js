c8b04cf4ef9018356771945706b9f75a6f3f7a5f
unknown <megagravity.dsz@gmail.com>
2022-02-16 10:49:49 +0530

    Modded NAvBar

--- tosm/src/Components/Navbar/Navbar.js
+++ tosm/src/Components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import logo from '../tos.PNG'
 import { Navbar, Nav, Container, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
-
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function NavBar() {
   return (
     <div>
-         <Navbar bg="text-white bg-dark" expand="lg">
+         <Navbar className="navbar-bgnd" expand="lg">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Brand href="#" ><img className="Nav__logo" src={logo} alt=""/></Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
@@ -16,16 +17,18 @@ function NavBar() {
               navbarScroll
             >
               <Nav.Link href="#action1" className="Nav__link">Home</Nav.Link>
-              <Nav.Link href="#action2"  className="Nav__link">about</Nav.Link>
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <Nav.Link href="#action2"  className="Nav__link">About</Nav.Link>
+              
+              <NavDropdown title="Link" id="navbarScrollingDropdown"  >
+                <NavDropdown.Item href="#action3" >Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
+                <NavDropdown.Item href="#action5"  >
                   Something else here
                 </NavDropdown.Item>
+                
               </NavDropdown>
-              <Nav.Link href="#" disabled>
+              <Nav.Link href="#" disabled className="Nav__link" >
                 Link
               </Nav.Link>
             </Nav>
@@ -36,8 +39,10 @@ function NavBar() {
                 className="me-2"
                 aria-label="Search"
               />
-              <Button variant="outline-success">Search</Button>
+              <Button  className="Nav__sbutton" variant="success">Search</Button>
             </Form>
+            <ShoppingCartIcon className="Nav__cart"/>
+            <span className="header__optionLineTwo header__basketCount"> 1</span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
